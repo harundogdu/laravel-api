92e284fc8cf548e20f0e2a475d479cf2c77794cb
Harun Doğdu <harundogdu06@gmail.com>
2022-02-10 23:52:24 +0300

    product api controller created

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResources([
+    "products" => \App\Http\Controllers\Api\ProductController::class
+]);
