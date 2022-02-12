30d3bdd29bae5b156d8576f104f4b8574571b149
Harun Doğdu <harundogdu06@gmail.com>
2022-02-12 15:24:52 +0300

    return type data changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //JsonResource::withoutWrapping(); // Tüm Resource'lerde data objesi olarak döndürmemeyi sağlar
     }
 }
