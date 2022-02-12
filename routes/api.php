5517c84516fd06b249d12f3d467bc611e934dcfd
Harun Doğdu <harundogdu06@gmail.com>
2022-02-12 15:23:26 +0300

    some routes added

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Route::get('/products/custom', \App\Http\Controllers\Api\ProductController::class . '@custom');
+//Route::get('/products/custom2', \App\Http\Controllers\Api\ProductController::class . '@custom2');
+//Route::get('/products/custom3', \App\Http\Controllers\Api\ProductController::class . '@custom3');
+//Route::get('/products/listwithcategories', \App\Http\Controllers\Api\ProductController::class . '@listWithCategories');
+//Route::get('/categories/custom', \App\Http\Controllers\Api\CategoryController::class . '@custom');
+//Route::get('/users/custom', \App\Http\Controllers\Api\UserController::class . '@custom');
+
 
 Route::apiResources([
     "products" => \App\Http\Controllers\Api\ProductController::class,
